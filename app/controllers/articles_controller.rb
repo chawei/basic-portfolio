50f48cbca741a29f143025d71027a07a52e7e277
David Chawei Hsu <chawei.hsu@gmail.com>
2011-01-30 20:16:49 -0800

    apply rich editor to textarea

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.xml
   def show
-    @article = Article.find(params[:id])
+    @article = Article.published.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
